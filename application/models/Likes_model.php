45c30e0fced30d83a9ff97fe512772addef7f702
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-04-24 09:04:02 +0300

    Добавил функционал лайков новостей Адрес запроса для лайка - /news/like/@entity_id Адрес запроса для дизлайка - /news/unlike/@entity_id @entity_id - ID новости Защиту от накрутки не добавлял

---
+++ application/models/Likes_model.php
@@ -0,0 +1,117 @@
+<?php
+
+
+class Likes_model extends MY_Model
+{
+    const TABLE = 'likes';
+    
+    protected $id;
+    protected $entity_id;
+    protected $entity_type;
+    protected $counter;
+    
+    
+    
+    function __construct($id = false)
+    {
+        parent::__construct();
+        $this->class_table = self::TABLE;
+        $this->set_id($id);
+    }
+    
+    /**
+     * @param int $entity_id
+     * @param     $entity_type
+     * @param int $change
+     *
+     * @return int
+     * @throws Exception
+     */
+    public static function changeCounter(int $entity_id, $entity_type, int $change)
+    {
+        self::validate(compact('entity_id','entity_type','change'));
+        
+        $id = self::getId($entity_id, $entity_type);
+        $counter = self::getCounterById($id)+$change;
+        $CI =& get_instance();
+        $CI->s->from(self::TABLE)
+           ->where('id',$id)
+           ->update(['counter'=>$counter])
+           ->execute();
+        return $counter;
+    }
+    
+    /**
+     * @param $params
+     *
+     * @return bool
+     * @throws Exception
+     */
+    protected static function validate($params)
+    {
+        if ( $params['entity_id'] < 1) {
+            throw new Exception('Wrong entity_id');
+        }
+        if ( empty($params['entity_type']) || ! in_array($params['entity_type'],['news','comments'])) {
+            throw new Exception('Wrong entity_type');
+        }
+        if ( empty($params['change']) || ! in_array($params['change'],['1','-1'])) {
+            throw new Exception('Wrong change');
+        }
+        return true;
+    }
+    
+    /**
+     * @param $entity_id
+     * @param $entity_type
+     *
+     * @return bool|integer
+     * @throws Exception
+     */
+    protected static function getId($entity_id, $entity_type)
+    {
+        $CI =& get_instance();
+        $id = $CI->s->from(self::TABLE)
+              ->where('entity_id',$entity_id)
+              ->where('entity_type',$entity_type)
+              ->value('id');
+        if ( ! $id) {
+            $id = self::create(compact('entity_id','entity_type'));
+        }
+        if ( ! $id) {
+            throw new Exception('Error save like record');
+        }
+        return $id;
+    }
+    
+    /**
+     * @param $id
+     *
+     * @return int
+     */
+    protected static function getCounterById($id)
+    {
+        $CI =& get_instance();
+        $counter = $CI->s->from(self::TABLE)
+                    ->where('id',$id)
+                    ->value('counter');
+        return (int)$counter;
+    }
+    
+    /**
+     * @param $data
+     *
+     * @return bool
+     */
+    protected static function create($data)
+    {
+        $CI  =& get_instance();
+        $res = $CI->s->from(self::TABLE)->insert($data)->execute();
+        if ( ! $res) {
+            return false;
+        }
+    
+        return $CI->s->insert_id;
+    }
+    
+}
--- application/models/News_model.php
+++ application/models/News_model.php
@@ -10,7 +10,9 @@ class News_model extends MY_Model
 {
     const NEWS_TABLE = APPLICATION_NEWS;
     const PAGE_LIMIT = 5;
-
+    const LATEST_LIMIT = 3;
+    const DESC_LIMIT = 300;
+    
     protected $id;
     protected $header;
     protected $short_description;
@@ -19,19 +21,19 @@ class News_model extends MY_Model
     protected $tags;
     protected $time_created;
     protected $time_updated;
-
+    
     protected $views;
-
+    
     protected $comments;
     protected $likes;
-
-    function __construct($id = FALSE)
+    
+    function __construct($id = false)
     {
         parent::__construct();
         $this->class_table = self::NEWS_TABLE;
         $this->set_id($id);
     }
-
+    
     /**
      * @return string
      */
@@ -39,33 +41,40 @@ class News_model extends MY_Model
     {
         return $this->header;
     }
-
+    
     /**
      * @param mixed $header
      */
     public function set_header($header)
     {
         $this->header = $header;
+        
         return $this->_save('header', $header);
     }
-
+    
     /**
      * @return string
      */
     public function get_short_description()
     {
-        return $this->short_description;
+        if (!self::DESC_LIMIT) {
+            return $this->short_description;
+        }
+        $ci = get_instance();
+        $ci->load->helper('text');
+        return character_limiter($this->short_description,self::DESC_LIMIT);
     }
-
+    
     /**
      * @param mixed $description
      */
     public function set_short_description($description)
     {
         $this->short_description = $description;
+        
         return $this->_save('short_description', $description);
     }
-
+    
     /**
      * @return string
      */
@@ -73,8 +82,7 @@ class News_model extends MY_Model
     {
         return $this->text;
     }
-
-
+    
     /**
      * @return mixed
      */
@@ -82,16 +90,17 @@ class News_model extends MY_Model
     {
         return $this->img;
     }
-
+    
     /**
      * @param mixed $image
      */
     public function set_image($image)
     {
         $this->img = $image;
+        
         return $this->_save('image', $image);
     }
-
+    
     /**
      * @return string
      */
@@ -99,16 +108,17 @@ class News_model extends MY_Model
     {
         return $this->tags;
     }
-
+    
     /**
      * @param mixed $tags
      */
     public function set_tags($tags)
     {
         $this->tags = $tags;
+        
         return $this->_save('tags', $tags);
     }
-
+    
     /**
      * @return mixed
      */
@@ -116,16 +126,17 @@ class News_model extends MY_Model
     {
         return $this->time_created;
     }
-
+    
     /**
      * @param mixed $time_created
      */
     public function set_time_created($time_created)
     {
         $this->time_created = $time_created;
+        
         return $this->_save('time_created', $time_created);
     }
-
+    
     /**
      * @return int
      */
@@ -133,16 +144,17 @@ class News_model extends MY_Model
     {
         return strtotime($this->time_updated);
     }
-
+    
     /**
      * @param mixed $time_updated
      */
     public function set_time_updated($time_updated)
     {
         $this->time_updated = $time_updated;
+        
         return $this->_save('time_updated', $time_updated);
     }
-
+    
     /**
      * @return News_like_model
      */
@@ -150,7 +162,7 @@ class News_model extends MY_Model
     {
         return $this->likes;
     }
-
+    
     /**
      * @return News_comments_model[]
      */
@@ -158,34 +170,59 @@ class News_model extends MY_Model
     {
         return $this->comments;
     }
-
+    
     /**
-     * @param int $page
+     * @param int         $page
      * @param bool|string $preparation
+     *
      * @return array
      */
-    public static function get_all($preparation = FALSE)
+    public static function get_all($preparation = false)
     {
-
+        
         $CI =& get_instance();
-
+        
         $_data = $CI->s->from(self::NEWS_TABLE)->many();
-
+        
         $news_list = [];
         foreach ($_data as $_item) {
             $news_list[] = (new self())->load_data($_item);
         }
-
-        if ($preparation === FALSE) {
+        
+        if ($preparation === false) {
             return $news_list;
         }
-
+        
         return self::preparation($news_list, $preparation);
     }
-
+    
+    public static function getLatest($preparation = false)
+    {
+        $CI =& get_instance();
+        
+        $_data = $CI->s->from(self::NEWS_TABLE)
+                       ->sortDesc('id')
+                       ->limit(self::LATEST_LIMIT)
+                       ->select([
+                           'id',
+                           'header',
+                           'short_description',
+                           'img',
+                           'time_updated',
+                       ])->many();
+        $news_list = [];
+        foreach ($_data as $_item) {
+            $news_list[] = (new self())->load_data($_item);
+        }
+        if ($preparation === false) {
+            return $news_list;
+        }
+        return self::preparation($news_list, $preparation);
+    }
+    
     public static function preparation($data, $preparation)
     {
-
+        
         switch ($preparation) {
             case 'short_info':
                 return self::_preparation_short_info($data);
@@ -193,36 +230,38 @@ class News_model extends MY_Model
                 throw new Exception('undefined preparation type');
         }
     }
-
+    
     /**
      * @param News_model[] $data
+     *
      * @return array
      */
     private static function _preparation_short_info($data)
     {
         $res = [];
         foreach ($data as $item) {
-            $_info = new stdClass();
-            $_info->id = (int)$item->get_id();
-            $_info->header = $item->get_header();
+            $_info              = new stdClass();
+            $_info->id          = (int)$item->get_id();
+            $_info->header      = $item->get_header();
             $_info->description = $item->get_short_description();
-            $_info->img = $item->get_image();
-            $_info->time = $item->get_time_updated();
-            $res[] = $_info;
+            $_info->img         = $item->get_image();
+            $_info->time        = $item->get_time_updated();
+            $res[]              = $_info;
         }
+        
         return $res;
     }
     
-    
-    public static function create($data){
-
-        $CI =& get_instance();
-	    $res = $CI->s->from(self::NEWS_TABLE)->insert($_insert_data)->execute();
-	    if(!$res){
-	        return FALSE;
+    public static function create($data)
+    {
+        
+        $CI  =& get_instance();
+        $res = $CI->s->from(self::NEWS_TABLE)->insert($_insert_data)->execute();
+        if ( ! $res) {
+            return false;
         }
-	    return new self($CI->s->insert_id);
+        
+        return new self($CI->s->insert_id);
     }
     
-
 }
